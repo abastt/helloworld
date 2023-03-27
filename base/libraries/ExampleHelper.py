415726ef7b669e7a2914abd4e1d14c559a12b546
Andres Baston <abastt@gmail.com>
2023-03-27 17:54:02 -0300

    Update ExampleHelper.py

--- base/libraries/ExampleHelper.py
+++ base/libraries/ExampleHelper.py
@@ -4,7 +4,6 @@ from decouple import config
 from robot.api import logger
 
 def get_environment(var):
-    def get_environment(var):
     env = os.getenv(var)
     if env is None:
         return config(var)
