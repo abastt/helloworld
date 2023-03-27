07cd2fee685725dd1f724b29cd04d28951aecd4c
Andres Baston <abastt@gmail.com>
2023-03-27 18:05:55 -0300

    Update ExampleHelper.py

--- base/libraries/ExampleHelper.py
+++ base/libraries/ExampleHelper.py
@@ -1,13 +1,10 @@
 from time import sleep
-from robot.libraries.BuiltIn import BuiltIn
+import os
 from decouple import config
 from robot.api import logger
 
 def get_environment(var):
-    env = BuiltIn().get_variable_value("${var}")
-    if env is None:
-        return config(var)
-    return env
+    return config(var)
 
 
 def example_python_keyword():
